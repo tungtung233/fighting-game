3addc9d4f2f2713ed2b0dba70fbe950b289f24e2
Veronica Leung <tungtungleung233@hotmail.com>
2022-04-03 20:23:13 -0400

    Add player left and right movement

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ class Sprite {
 
   update() {
     this.draw();
+
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -62,3 +64,27 @@ function animate() {
 }
 
 animate();
+
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+      player.velocity.x = 1;
+      break;
+    case 'a':
+      player.velocity.x = -1;
+      break;
+  }
+  console.log(event.key);
+});
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      player.velocity.x = 0;
+      break;
+    case 'a':
+      player.velocity.x = 0;
+      break;
+  }
+  console.log(event.key);
+});
