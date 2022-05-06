63e21cf3803dd79ace702932e26c88360b4439cf
Veronica Leung <tungtungleung233@hotmail.com>
2022-05-06 15:48:16 -0400

    Add enemy attack collision detection

--- index.js
+++ index.js
@@ -89,6 +89,18 @@ const keys = {
   },
 };
 
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
+
 function animate() {
   window.requestAnimationFrame(animate);
   context.fillStyle = 'black';
@@ -114,13 +126,12 @@ function animate() {
   }
 
   // detect for collision via attackBox
-  if (
-    player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-    player.attackBox.position.x <= enemy.position.x + enemy.width &&
-    player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-    player.attackBox.position.y <= enemy.position.y + enemy.height
-  ) {
-    console.log('hit');
+  if (rectangularCollision({ rectangle1: player, rectangle2: enemy })) {
+    console.log('enemy hit');
+  }
+
+  if (rectangularCollision({ rectangle1: enemy, rectangle2: player })) {
+    console.log('player hit');
   }
 }
 
